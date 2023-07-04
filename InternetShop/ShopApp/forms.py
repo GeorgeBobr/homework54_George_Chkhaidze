854b0edb1ee5bb01ce18ca101a1160d677e07771
Georg <georgestoneix@gmail.com>
2023-07-04 05:38:20 -0700

    fix errors

--- InternetShop/ShopApp/forms.py
+++ InternetShop/ShopApp/forms.py
@@ -1,11 +1,12 @@
 from django import forms
-from django.forms import widgets
+from ShopApp.models import Category
 
 class CategoryForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label="Название")
     description = forms.CharField(max_length=2000, required=True, label='Описание')
 class ProductForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label="Название")
-    category = forms.ChoiceField(required=True, label="Категория")
-    price = forms.CharField(required=True, label="Цена")
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=True, label="Категория")
+    remainder = forms.IntegerField(required=True, label="Остаток")
+    price = forms.CharField(required=True, label="Цена$")
     image = forms.URLField(required=True, label="Картинка")
