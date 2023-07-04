9196a767f3b893e6366c6f09a32a5a26392aa0de
Georg <georgestoneix@gmail.com>
2023-07-04 05:12:40 -0700

    delete class ProductAdmin

--- InternetShop/ShopApp/admin.py
+++ InternetShop/ShopApp/admin.py
@@ -2,13 +2,4 @@ from django.contrib import admin
 from ShopApp.models import Product, Category
 
 admin.site.register(Category)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'price', 'created_at']
-    list_display_links = ['id', 'title']
-    list_filter = ['price']
-    search_fields = ['title', 'description']
-    fields = ['title', 'price', 'content', 'created_at', 'category']
-    readonly_fields = ['created_at']
-
-
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Product)
