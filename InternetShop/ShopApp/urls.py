2fae94e6a679c1f365952dc1484512ab7e152e60
Georg <georgestoneix@gmail.com>
2023-07-04 04:40:37 -0700

    add some urls

--- InternetShop/ShopApp/urls.py
+++ InternetShop/ShopApp/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-from .views import product_view, products_view, product_add_view, category_add_view, category_edit_view, \
-    categories_view, delete_category, delete_product, product_edit_view
+from .views import product_view, products_view, add_product, edit_category, add_category, \
+    categories_view, delete_category, delete_product, edit_product
 urlpatterns = [
-    path('', products_view, name='products_view'),
-    path('products', products_view, name='products_view'),
-    path('products/<int:id>/', product_view, name='product_view'),
+    path('', products_view, name='products'),
+    path('products', products_view, name='product'),
+
+    path('products/<int:id>/', product_view, name='product'),
     path('products/<int:id>/delete', delete_product, name='delete_product'),
-    path('products/<int:id>/edit', product_edit_view, name='product_edit_view'),
-    path('categories/add/', category_add_view, name='category_add_view'),
-    path('products/add/', product_add_view, name='product_add_view'),
-    path('categories/', categories_view, name='categories_view'),
+    path('products/<int:id>/edit', edit_product, name='edit_product'),
+
+    path('categories/add/', add_category, name='add_category'),
+    path('products/add/', add_product, name='add_product'),
+
+    path('categories/', categories_view, name='categories'),
     path('categories/<int:id>/delete/', delete_category, name='delete_category'),
-    path('categories/<int:id>/edit/', category_edit_view, name='category_edit_view')
+    path('categories/<int:id>/edit/', edit_category, name='edit_category')
 
 ]
