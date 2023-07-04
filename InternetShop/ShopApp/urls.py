24e7a678e49d97d4ccf600a416ea3a7ef238fe16
Georg <georgestoneix@gmail.com>
2023-07-04 05:52:14 -0700

    fix bugs, edit html pages

--- InternetShop/ShopApp/urls.py
+++ InternetShop/ShopApp/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path
-from .views import product_view, products_view, add_product, edit_category, add_category, \
+from .views import product_detail, products_view, add_product, add_category, \
     categories_view, delete_category, delete_product, edit_product
 urlpatterns = [
     path('', products_view, name='products'),
-    path('products', products_view, name='product'),
 
-    path('products/<int:id>/', product_view, name='product'),
+    path('products/<int:id>/', product_detail, name='product_detail'),
     path('products/<int:id>/delete', delete_product, name='delete_product'),
     path('products/<int:id>/edit', edit_product, name='edit_product'),
-
-    path('categories/add/', add_category, name='add_category'),
     path('products/add/', add_product, name='add_product'),
 
     path('categories/', categories_view, name='categories'),
     path('categories/<int:id>/delete/', delete_category, name='delete_category'),
-    path('categories/<int:id>/edit/', edit_category, name='edit_category')
-
+    path('categories/add/', add_category, name='add_category'),
 ]
--- InternetShop/ShopApp/views.py
+++ InternetShop/ShopApp/views.py
@@ -1,80 +1,85 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from ShopApp.models import Product
 from ShopApp.models import Category
-from django.http import HttpResponseRedirect, Http404, HttpResponseNotFound
-
+from ShopApp.forms import ProductForm, CategoryForm
 def products_view(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, "products.html", context)
 
-def product_view(request, id):
+def product_detail(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'product.html', {'product': product})
 
 
-def category_add_view(request):
+def add_category(request):
     if request.method == "GET":
-        return render(request, "create_category.html")
-    if request.method == 'POST':
-        Category.objects.create(
-            title=request.POST.get('title'),
-            description=request.POST.get('description'),
-        )
-        return HttpResponseRedirect("/")
+        form = CategoryForm()
+        return render(request, "add_category.html", {"form": form})
+    else:
+        form = CategoryForm(data=request.POST)
+        if form.is_valid():
+            Category.objects.create(title=form.cleaned_data.get('title'),
+                                    description=form.cleaned_data.get('description'))
+            return redirect('categories')
+        else:
+            print(form.errors)
+            return render(request, "add_category.html", {"form": form})
 
-def product_add_view (request):
+def add_product(request):
     if request.method == "GET":
-        categories = Category.objects.all()
-        context = {'categories': categories}
-        return render(request, "create_product.html", context)
-    elif request.method == 'POST':
-        category = get_object_or_404(Category, id=request.POST.get('category'))
-        Product.objects.create(
-            title=request.POST.get('title'),
-            description=request.POST.get('description'),
-            category=category,
-            created_at=request.POST.get('created_at'),
-            price=request.POST.get('price'),
-            image=request.POST.get('image')
-        )
-        return HttpResponseRedirect("/")
+        form = ProductForm()
+        return render(request, "add_product.html", {"form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            Product.objects.create(title=form.cleaned_data.get('title'),
+                                   category=form.cleaned_data.get('category'),
+                                   price=form.cleaned_data.get('price'),
+                                   remainder=form.cleaned_data.get('remainder'),
+                                   image=form.cleaned_data.get('image'))
+            return redirect('products')
+        else:
+            print(form.errors)
+            return render(request, "add_product.html", {"form": form})
 
 def categories_view(request):
-    categories = get_object_or_404(Product, id=id)
+    categories = Category.objects.all()
     context = {'categories': categories}
     return render(request, "categories.html", context)
 def delete_category(request, id):
     category = get_object_or_404(Category, id=id)
     category.delete()
-    return HttpResponseRedirect("/")
-
-def category_edit_view(request, id):
-    category = get_object_or_404(Category, id=id)
-    if request.method == 'POST':
-        category.title = request.POST['title']
-        category.description = request.POST['description']
-        category.save()
-        return redirect('categories_view')
-    return render(request, 'category_edit.html', {'category': category})
-
+    return redirect("categories")
 
 def delete_product(request, id):
     product = get_object_or_404(Product, id=id)
-    product.delete()
-    return HttpResponseRedirect("/")
+    if request.method == "GET":
+        return render(request, "delete_product.html", {"product": product})
+    else:
+        product.delete()
+        return redirect("products_view")
 
-def product_edit_view(request, id):
+def edit_product(request, id):
     product = get_object_or_404(Product, id=id)
-    categories = Category.objects.all()
-    if request.method == 'POST':
-        product.title = request.POST['title']
-        product.description = request.POST['description']
-        category_id = int(request.POST.get('category'))
-        category = get_object_or_404(Category, id=category_id)
-        product.category = category
-        product.price = request.POST.get('price')
-        product.image = request.POST.get('image')
-        product.save()
-        return redirect('product_view', id=id)
-    return render(request, 'product_edit.html', {'product': product, 'categories': categories})
+    if request.method == "GET":
+        form = ProductForm(initial={
+            "title": product.title,
+            "category": product.category,
+            "price": product.price,
+            "image": product.image
+        })
+        return render(request, "edit_product.html", {"form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.title = request.POST.get('title')
+            product.category = request.POST.get("category")
+            product.price = request.POST.get("price")
+            product.remainder = request.POST.get("remainder")
+            product.image = request.POST.get("image")
+            product.save()
+            return redirect("products_views")
+        else:
+            return render(request, "edit_product.html", {"form": form})
+
