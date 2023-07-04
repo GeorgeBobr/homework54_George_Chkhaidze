074a972396ba29d8d0c662954d4dc440c533bcb2
Georg <georgestoneix@gmail.com>
2023-07-04 03:33:21 -0700

    add forms

---
+++ InternetShop/ShopApp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import widgets
+
+class CategoryForm(forms.Form):
+    title = forms.CharField(max_length=50, required=True, label="Название")
+    description = forms.CharField(max_length=2000, required=True, label='Описание')
+class ProductForm(forms.Form):
+    title = forms.CharField(max_length=50, required=True, label="Название")
+    category = forms.ChoiceField(required=True, label="Категория")
+    price = forms.CharField(required=True, label="Цена")
+    image = forms.CharField(required=True, label="Картинка")
